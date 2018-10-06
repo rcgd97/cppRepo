32d2b1e365c05946d7a6025946fb1364b7acf91f
rgsax <riccardog.sax@gmail.com>
2018-10-06 10:39:37 +0200

    Create Graph.hpp

---
+++ headers/Graph.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+//graph class for simple operation
+
+class Graph {
+private:
+	unsigned *ids = nullptr;
+	bool **edges = nullptr;
+public:
+	Graph(int num_ids=0);
+	~Graph();
+	bool operator()(int i, int j);
+	void operator()(int i, int j, bool state);
+};
